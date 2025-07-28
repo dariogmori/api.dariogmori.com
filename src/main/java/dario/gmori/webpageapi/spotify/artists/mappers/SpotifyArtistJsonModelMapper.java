f6790eec4cec7e38aed3f338818f607c3686c6fa
Dario G. Mori <dario@resiz.es>
2025-07-28 17:15:38 +0200

    fix: smallest image size for artists

--- src/main/java/dario/gmori/webpageapi/spotify/artists/mappers/SpotifyArtistJsonModelMapper.java
+++ src/main/java/dario/gmori/webpageapi/spotify/artists/mappers/SpotifyArtistJsonModelMapper.java
@@ -5,13 +5,23 @@ import dario.gmori.webpageapi.spotify.artists.Artist;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.function.Function;
 
 @Service
 public class SpotifyArtistJsonModelMapper implements Function<JsonNode, List<Artist>> {
+    private String getCoverImageWithSmallestSize(JsonNode artist)
+    {
+        if(artist.has("images")){
+            if (artist.get("images").size() == 1) {
+                return artist.get("images").get(0).get("url").asText();
+            } else {
+                return artist.get("images").get(artist.get("images").size() - 1).get("url").asText(); // Return the smallest image
+            }
+        }else {
+            return null; // Return an empty string if no images are available
+        }
+    }
     @Override
     public List<Artist> apply(JsonNode jsonNode) {
         List<Artist> artists = new ArrayList<>();
@@ -19,7 +29,7 @@ public class SpotifyArtistJsonModelMapper implements Function<JsonNode, List<Art
         jsonNode.forEach(artist -> {
             artists.add(Artist.builder()
                     .name(artist.get("name").asText())
-                    .avatarUrl(artist.has("images") ? artist.get("images").get(0).get("url").asText() : null)
+                    .avatarUrl(getCoverImageWithSmallestSize(artist))
                     .profileUrl(artist.get("external_urls").get("spotify").asText())
                     .followers(artist.has("followers") ? artist.get("followers").get("total").asInt() : 0)
                     .genres(artist.has("genres") ? artist.get("genres").toString() : "")
